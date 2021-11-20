e5f41dff5cc3655d05dd63e5b25e2c4895c3bfcf
Samrat Dhital <34317197+samratdhital@users.noreply.github.com>
2021-11-20 22:26:15 +0545

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -5,7 +5,7 @@ const redo = document.getElementById("redo");
 const save = document.getElementById("save");
 const textArea = document.getElementById("text-area");
 
-// event listner for clear text area
+// event listener for clear text area
 clearTextArea.addEventListener("click", () => {
   textArea.value = "";
 });
@@ -13,12 +13,12 @@ clearTextArea.addEventListener("click", () => {
 //❤️Text area gets automatically focus when the page is loaded for the first time.
 textArea.focus();
 
-//❤️Text area gets automatically focus when the 'New' list element is clicked!.
-clearTextArea.addEventListener("click", function () {
+//❤️Text area gets automatically focus when the "New" list element is clicked!.
+clearTextArea.addEventListener("click", () => {
   textArea.focus();
 });
 
-// event listner for input in  text area
+// event listener for input in  text area
 textArea.addEventListener("input", (e) => {
   textArea.value = e.target.value;
 });
